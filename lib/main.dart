1e54f566dc89770ebf080d3eddbad48f449546ed
flutter <akashvs64731@gmail.com>
2023-09-28 14:39:19 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:deal_test/view/screens/loginhome.dart';
+import 'package:deal_test/view/screens/settingpage.dart';
+
 import 'package:deal_test/view/screens/splashscreen.dart';
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -10,9 +15,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        '/':(context) => const SplashScreen(),
+        '/': (context) => const SplashScreen(),
+        '/loginhome': (context) => const Loginhome(),
+        '/settingspage': (context) => const SettingsPage(),
       },
-      initialRoute: '/',
+      initialRoute: '/settingspage',
     );
   }
 }
---
+++ lib/view/screens/loginhome.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+class Loginhome extends StatefulWidget {
+  const Loginhome({super.key});
+
+  @override
+  State<Loginhome> createState() => _LoginhomeState();
+}
+
+class _LoginhomeState extends State<Loginhome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Image(image: AssetImage('assets/images/applogo.png')),
+        elevation: 0,
+      ),
+      body: Stack(
+        fit: StackFit.expand ,
+        children: [
+          Container(
+            color: Colors.white,
+          ),
+           Column(
+           children: [
+             const Expanded(flex:2,
+                 child:Column(
+                   children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 90),
+                      child: Image(image: AssetImage('assets/images/toppng 1.png')),
+                    ),
+
+                   ],
+                 ) ),
+             Expanded(
+               flex: 1,
+                 child:Column(
+                   children: [
+                     const Padding(
+                       padding: EdgeInsets.only(left: 30),
+                       child: Text(
+                         "Catch All Deal Is \nEasier With One Tap",
+                         style: TextStyle(
+                           fontFamily: 'Oswald-Bold',
+                             color: Color(0xFF4A4A5F),
+                           fontSize: 30,
+                           fontWeight: FontWeight.bold,
+                         ),
+                       ),
+                     ),
+                     Padding(padding: EdgeInsets.only(top:30),
+                     child: Row(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       children: [
+                         SizedBox(
+                           width: 133,
+                           height: 46,
+                           child: ElevatedButton(
+                               style: ButtonStyle(
+                                 backgroundColor: MaterialStatePropertyAll(Color(0xFF5529A4)),
+                               ),
+                               onPressed: () {},
+                               child:Text("Sign In",
+                                 style:TextStyle(fontFamily: 'Oswald-Bold'))
+                           ),
+                         ),
+                         SizedBox(width: 16),
+                         SizedBox(width: 133,
+                           height: 46,
+                           child: ElevatedButton(
+                               style: ButtonStyle(
+                                 backgroundColor:
+                                   MaterialStatePropertyAll(Color(0xFF5529A4)),
+                               ),
+                               onPressed:() {
+
+                               },  child:Text("Register",style: TextStyle(
+                               fontFamily:'Oswald-Bold' ),) ),
+                         ),
+                       ],
+                     ),
+                     )
+                   ],
+                 ),
+             ),
+           ],
+          )
+        ],
+      ),
+    );
+  }
+}
